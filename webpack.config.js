45e12fe7acbfdcd58255d3b06dfbba034ba0709a
Thiago Bignotto <bignotto@gmail.com>
2021-04-14 07:25:22 -0300

    Project configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+    mode: isDev? 'development' : 'production',
+    devtool: isDev? 'eval-source-map': 'source-map',
     entry: path.resolve(__dirname,'src','index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname,'public','index.html')
+        })
+    ],
     resolve: {
         extensions: ['.js','.jsx']
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname,'public')
     },
     module: {
         rules: [
@@ -15,6 +28,11 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader','css-loader','sass-loader']
             }
         ]
     }
