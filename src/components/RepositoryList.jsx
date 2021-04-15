bc6afe5b94c53f14b00b93ba8d35893ca1ee6078
Thiago Bignotto <bignotto@gmail.com>
2021-04-15 06:32:41 -0300

    save commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,39 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios:</h1>
+            <ul>
+                <li>
+                    <strong>repo name</strong>
+                    <p>the longest repo description you have ever, ever read, and still it isn't too long.</p>
+                    <a href="">link to repo</a>
+                </li>
+                <li>
+                    <strong>repo name</strong>
+                    <p>the longest repo description you have ever, ever read, and still it isn't too long.</p>
+                    <a href="">link to repo</a>
+                </li>
+                <li>
+                    <strong>repo name</strong>
+                    <p>the longest repo description you have ever, ever read, and still it isn't too long.</p>
+                    <a href="">link to repo</a>
+                </li>
+                <li>
+                    <strong>repo name</strong>
+                    <p>the longest repo description you have ever, ever read, and still it isn't too long.</p>
+                    <a href="">link to repo</a>
+                </li>
+                <li>
+                    <strong>repo name</strong>
+                    <p>the longest repo description you have ever, ever read, and still it isn't too long.</p>
+                    <a href="">link to repo</a>
+                </li>
+                <li>
+                    <strong>repo name</strong>
+                    <p>the longest repo description you have ever, ever read, and still it isn't too long.</p>
+                    <a href="">link to repo</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
