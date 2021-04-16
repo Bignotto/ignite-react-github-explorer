44ff95b68154acabecee268f6c234c1fb529ce36
Thiago Bignotto <bignotto@gmail.com>
2021-04-16 08:12:23 -0300

    Listing Github repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,25 +1,41 @@
-import { RepositoryItem } from "./RepositoryItem";
+import { useEffect, useState } from 'react';
+import { RepositoryItem } from './RepositoryItem';
 import '../styles/repository.scss';
 
+//https://api.github.com/users/Bignotto/repos
+
 const repository = {
-    name: 'James',
-    description: "James is a reporting robot.",
-    link: 'https://github.com/bignotto/james'
-}
+  name: 'James',
+  description: 'James is a reporting robot.',
+  link: 'https://github.com/bignotto/james',
+};
 
 export function RepositoryList() {
-    return (
-        <section className="repository-list">
-            <h1>Lista de repositórios:</h1>
-            <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+  const [repositories, setRepositories] = useState([]);
 
-            </ul>
-        </section>
-    )
+  useEffect(() => {
+    fetch('https://api.github.com/users/Bignotto/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios:</h1>
+      <ul>
+        {repositories.map(item => {
+          return (
+            <RepositoryItem
+              key={item.name}
+              repository={{
+                name: item.name,
+                description: item.description,
+                link: item.html_url,
+              }}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
